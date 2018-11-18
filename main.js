5a51bee06df82a4f1f0152e615e54fa8c0aa5c65
Galib Hasanov <galibhasanov98@gmail.com>
2018-11-18 11:42:55 +0400

    first commit

---
+++ main.js
@@ -0,0 +1,156 @@
+let a = new Audio('sound/a.mp3');
+a.loop = false;
+document.querySelector('#a').addEventListener('click', function(){
+    a.play();
+});
+
+let b = new Audio('sound/b.mp3');
+b.loop = false;
+document.querySelector('#b').addEventListener('click', function(){
+    b.play();
+});
+
+let c = new Audio('sound/c.mp3');
+c.loop = false;
+document.querySelector('#c').addEventListener('click', function(){
+    c.play();
+});
+
+let d = new Audio('sound/d.mp3');
+d.loop = false;
+document.querySelector('#d').addEventListener('click', function(){
+    d.play();
+});
+
+let e = new Audio('sound/e.mp3');
+e.loop = false;
+document.querySelector('#e').addEventListener('click', function(){
+    e.play();
+});
+
+let f = new Audio('sound/f.mp3');
+f.loop = false;
+document.querySelector('#f').addEventListener('click', function(){
+    f.play();
+});
+
+let g = new Audio('sound/g.mp3');
+g.loop = false;
+document.querySelector('#g').addEventListener('click', function(){
+    g.play();
+});
+
+let h = new Audio('sound/h.mp3');
+h.loop = false;
+document.querySelector('#h').addEventListener('click', function(){
+    h.play();
+});
+
+let i = new Audio('sound/i.mp3');
+a.loop = false;
+document.querySelector('#i').addEventListener('click', function(){
+    i.play();
+});
+
+let j = new Audio('sound/j.mp3');
+j.loop = false;
+document.querySelector('#j').addEventListener('click', function(){
+    j.play();
+});
+
+let k = new Audio('sound/k.mp3');
+k.loop = false;
+document.querySelector('#k').addEventListener('click', function(){
+    k.play();
+});
+
+let l = new Audio('sound/l.mp3');
+l.loop = false;
+document.querySelector('#l').addEventListener('click', function(){
+    l.play();
+});
+
+let m = new Audio('sound/m.mp3');
+m.loop = false;
+document.querySelector('#m').addEventListener('click', function(){
+    m.play();
+});
+
+let n = new Audio('sound/n.mp3');
+n.loop = false;
+document.querySelector('#n').addEventListener('click', function(){
+    n.play();
+});
+
+let o = new Audio('sound/o.mp3');
+o.loop = false;
+document.querySelector('#o').addEventListener('click', function(){
+    o.play();
+});
+
+let p = new Audio('sound/p.mp3');
+p.loop = false;
+document.querySelector('#p').addEventListener('click', function(){
+    p.play();
+});
+
+let q = new Audio('sound/q.mp3');
+q.loop = false;
+document.querySelector('#q').addEventListener('click', function(){
+    q.play();
+});
+
+let r = new Audio('sound/r.mp3');
+r.loop = false;
+document.querySelector('#r').addEventListener('click', function(){
+    r.play();
+});
+
+let s = new Audio('sound/s.mp3');
+s.loop = false;
+document.querySelector('#s').addEventListener('click', function(){
+    s.play();
+});
+
+let t = new Audio('sound/t.mp3');
+t.loop = false;
+document.querySelector('#t').addEventListener('click', function(){
+    t.play();
+});
+
+let u = new Audio('sound/u.mp3');
+u.loop = false;
+document.querySelector('#u').addEventListener('click', function(){
+    u.play();
+});
+
+let v = new Audio('sound/v.mp3');
+v.loop = false;
+document.querySelector('#v').addEventListener('click', function(){
+    v.play();
+});
+
+let w = new Audio('sound/w.mp3');
+w.loop = false;
+document.querySelector('#w').addEventListener('click', function(){
+    w.play();
+});
+
+let x = new Audio('sound/x.mp3');
+x.loop = false;
+document.querySelector('#x').addEventListener('click', function(){
+    x.play();
+});
+
+let y = new Audio('sound/y.mp3');
+y.loop = false;
+document.querySelector('#y').addEventListener('click', function(){
+    y.play();
+});
+
+let z = new Audio('sound/z.mp3');
+z.loop = false;
+document.querySelector('#z').addEventListener('click', function(){
+    z.play();
+});
+
